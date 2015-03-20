7aee8d7d446912134e25d03d8c1a30eab5ce6c4b
Miłosz Mazur <milosz.mazur@hotmail.com>
2015-03-20 14:14:28 +0100

    added failing with grace

--- hearthfetch.rb
+++ hearthfetch.rb
@@ -3,12 +3,15 @@ require 'uri'
 require 'json'
 helpers do 
     def get_card(name)
-        require 'nokogiri'
-        require 'open-uri'
-        doc = Nokogiri::HTML(open(URI.encode("http://hearthstone.wikia.com/wiki/Special:Search?search=#{name}&fulltext=Search&ns0=1&ns14=1#")))
-        stuff = doc.xpath("//a[@class='result-link']").map{|x| x.content}
-        card_url = Nokogiri::HTML(open(stuff[1])).xpath("//img").xpath("//div[@class='tooltip-content']")[0].children[0]["href"]
-        return JSON.generate({"text" => card_url})
+        begin
+            require 'nokogiri'
+            require 'open-uri'
+            doc = Nokogiri::HTML(open(URI.encode("http://hearthstone.wikia.com/wiki/Special:Search?search=#{name}&fulltext=Search&ns0=1&ns14=1#")))
+            stuff = doc.xpath("//a[@class='result-link']").map{|x| x.content}
+            card_url = Nokogiri::HTML(open(stuff[1])).xpath("//img").xpath("//div[@class='tooltip-content']")[0].children[0]["href"]
+            return JSON.generate({"text" => card_url})
+        rescue 
+            return JSON.generate({"text" => "lol nope"})
     end
 
 end
