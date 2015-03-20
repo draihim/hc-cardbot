3ed06d6811fafa3805b4db460aa83f37aeaa75b2
Miłosz Mazur <milosz.mazur@hotmail.com>
2015-03-20 14:27:30 +0100

    messed up the response

--- hearthfetch.rb
+++ hearthfetch.rb
@@ -9,9 +9,15 @@ helpers do
             doc = Nokogiri::HTML(open(URI.encode("http://hearthstone.wikia.com/wiki/Special:Search?search=#{name}&fulltext=Search&ns0=1&ns14=1#")))
             stuff = doc.xpath("//a[@class='result-link']").map{|x| x.content}
             card_url = Nokogiri::HTML(open(stuff[1])).xpath("//img").xpath("//div[@class='tooltip-content']")[0].children[0]["href"]
-            return JSON.generate(username: "pix", icon_emoji: ":neckbeard:" text: card_url})
+            return JSON.generate({username: "pix",
+                                  icon_emoji: ":neckbeard:",
+                                  text: card_url
+            })
         rescue 
-            return JSON.generate({username: "pix", icon_emoji: ":cry:" "text" => "lol nope"})
+            return JSON.generate({username: "pix",
+                                  icon_emoji: ":cry:",
+                                  text: "lol nope"
+            })
     end
 
 end
