665964cbdf82bc7e8604308675a796883abe3b67
Miłosz Mazur <milosz.mazur@hotmail.com>
2015-03-20 15:07:32 +0100

    refractor stuff and change bot icon

--- hearthfetch.rb
+++ hearthfetch.rb
@@ -1,31 +1,30 @@
 require 'sinatra'
 require 'uri'
 require 'json'
+require 'nokogiri'
+require 'open-uri'
+
 helpers do 
     def get_card(name)
         begin
-            require 'nokogiri'
-            require 'open-uri'
             doc = Nokogiri::HTML(open(URI.encode("http://hearthstone.wikia.com/wiki/Special:Search?search=#{name}&fulltext=Search&ns0=1&ns14=1#")))
             stuff = doc.xpath("//a[@class='result-link']").map{|x| x.content}
             card_url = Nokogiri::HTML(open(stuff[1])).xpath("//img").xpath("//div[@class='tooltip-content']")[0].children[0]["href"]
-            return JSON.generate({username: "pix",
-                                  icon_emoji: ":neckbeard:",
-                                  text: card_url
-            })
-        rescue 
-            return JSON.generate({username: "pix",
-                                  icon_emoji: ":cry:",
-                                  text: "lol nope"
-            })
         end
 
     end
 end
 
     post '/' do
-        puts params
-        trigger=params['trigger_word']
-        card = params['text'].scan(/#{trigger}(.*)/).flatten[0].strip
-        return get_card(card)
+        begin
+            trigger=params['trigger_word']
+            card = params['text'].scan(/#{trigger}(.*)/).flatten[0].strip
+            url = get_card(card)
+        rescue
+            url = "Not found. Did you mess something up? You did. You messed something up."
+            return JSON.generate({
+                username: "pix",
+                icon_emoji: ":shipit:",
+                text: url
+            }) 
     end
